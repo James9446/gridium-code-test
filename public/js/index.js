91e68b86b04f02bd499fc0b9d23f03b9714ad7ed
James Reynolds <jameshreynolds@gmail.com>
2017-09-20 15:05:10 -0700

    Added user variable to store user input. Added submit button and answerAll function that checks each input has been submitted. If not an alert comes up specifying which input is needed.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
+var user = {
+    employment_answer:"", 
+    experience_answer:"",
+    companysize_answer:"",
+    versioncontrol_answer:"",
+    issuetracker_answer:"",
+    buildserver_answer:"",
+    codeanalysis_answer:"",
+    knowledgerepo_answer:"",
+    traveloptions_answer:"",
+    schedule_answer:"",
+    remotework_answer:"",
+    relocation_answer:"",
+    operating_system_answer:"",
+    machine_type_answer:"",
+    monitor_answer:"",
+    level_answer:"",
+    training_type_answer:"",
+    pto_answer:""
+}
+
 // Employment type selection
 var employmentType = enumerate("Permanent", "Temporary", "Project");
 
@@ -13,6 +34,8 @@ for (value in employmentType.all)
 function displayEmploymentFrom(dropdown)
 {
   $("#employment_selection_output").text(employmentType.keys[dropdown.value]);
+  user.employment_answer = employmentType.keys[dropdown.value]
+  console.log(user.employment_answer)
 }
 
 // Employment type selection
@@ -30,6 +53,8 @@ for (value in experienceLevels.all)
 function displayExperienceFrom(dropdown)
 {
   $("#experience_selection_output").text(experienceLevels.keys[dropdown.value]);
+  user.experience_answer = experienceLevels.keys[dropdown.value]
+  console.log(user.experience_answer)
 }
 
 // Company Size Selection
@@ -47,6 +72,8 @@ for (value in companySize.all)
 function displayCompanySizeFrom(dropdown)
 {
   $("#company_size_selection_output").text(companySize.keys[dropdown.value]);
+  user.companysize_answer = companySize.keys[dropdown.value]
+  console.log(user.companysize_answer)
 }
 
 // Version Control Selection
@@ -64,6 +91,8 @@ for (value in versionControlSystem.all)
 function displayVersionControlSystemFrom(dropdown)
 {
   $("#version_control_selection_output").text(versionControlSystem.keys[dropdown.value]);
+  user.versioncontrol_answer = versionControlSystem.keys[dropdown.value]
+  console.log(user.versioncontrol_answer)
 }
 
 // Issue Tracker Selection
@@ -81,6 +110,8 @@ for (value in issueTrackers.all)
 function displayIssueTrackersFrom(dropdown)
 {
   $("#issue_tracker_selection_output").text(issueTrackers.keys[dropdown.value]);
+  user.issuetracker_answer = issueTrackers.keys[dropdown.value]
+  console.log(user.issuetracker_answer)
 }
 
 // Build Server Selection
@@ -98,6 +129,8 @@ for (value in buildServers.all)
 function displayBuildServersFrom(dropdown)
 {
   $("#build_server_selection_output").text(buildServers.keys[dropdown.value]);
+  user.buildserver_answer = buildServers.keys[dropdown.value]
+  console.log(user.buildserver_answer)
 }
 
 // Analysis Tool Selection
@@ -115,6 +148,8 @@ for (value in codeAnalysisTools.all)
 function displayCodeAnalysisToolsFrom(dropdown)
 {
   $("#code_analysis_tool_selection_output").text(codeAnalysisTools.keys[dropdown.value]);
+  user.codeanalysis_answer = codeAnalysisTools.keys[dropdown.value]
+  console.log(user.codeanalysis_answer)
 }
 
 // Knowledge Repository Selection
@@ -132,6 +167,8 @@ for (value in knowledgeRepos.all)
 function displayKnowledgeReposFrom(dropdown)
 {
   $("#knowledge_repo_selection_output").text(knowledgeRepos.keys[dropdown.value]);
+  user.knowledgerepo_answer = knowledgeRepos.keys[dropdown.value]
+  console.log(user.knowledgerepo_answer)
 }
 
 // Travel Options Selection
@@ -149,23 +186,27 @@ for (value in travelOptions.all)
 function displaytTavelOptionsFrom(dropdown)
 {
   $("#travel_options_selection_output").text(travelOptions.keys[dropdown.value]);
+  user.traveloptions_answer = travelOptions.keys[dropdown.value]
+  console.log(user.traveloptions_answer)
 }
 
 // Schedule Options Selection
-var travelOptions = enumerate("Fixed", "Flexible");
+var scheduleOptions = enumerate("Fixed", "Flexible");
 
 var label = $("#schedule_options_selection_label");
-label.append("<select id=\"schedule_options_selection_dropdown\" onchange=\"displayTravelOptionsFrom(this)\"></select>");
+label.append("<select id=\"schedule_options_selection_dropdown\" onchange=\"displayScheduleOptionsFrom(this)\"></select>");
 
 var dropdown = $("#schedule_options_selection_dropdown");
 dropdown.append("<option disabled selected>Select your schedule availability!</option>");
 
-for (value in travelOptions.all)
-    dropdown.append("<option value=\"" + value + "\">" + travelOptions.keys[value] + "</option>");
+for (value in scheduleOptions.all)
+    dropdown.append("<option value=\"" + value + "\">" + scheduleOptions.keys[value] + "</option>");
 
-function displayTravelOptionsFrom(dropdown)
+function displayScheduleOptionsFrom(dropdown)
 {
-  $("#schedule_options_selection_output").text(travelOptions.keys[dropdown.value]);
+  $("#schedule_options_selection_output").text(scheduleOptions.keys[dropdown.value]);
+  user.schedule_answer = scheduleOptions.keys[dropdown.value]
+  console.log(user.schedule_answer)
 }
 
 // Remote Work Selection Selection
@@ -183,6 +224,8 @@ for (value in remoteWorking.all)
 function displayRemoteWorkingFrom(dropdown)
 {
   $("#remote_work_selection_output").text(remoteWorking.keys[dropdown.value]);
+  user.remotework_answer = remoteWorking.keys[dropdown.value]
+  console.log(user.remotework_answer)
 }
 
 // Relocation Package Selection
@@ -200,6 +243,8 @@ for (value in relocationPackages.all)
 function displayRelocationPackagesFrom(dropdown)
 {
   $("#relocation_package_selection_output").text(relocationPackages.keys[dropdown.value]);
+  user.relocation_answer = relocationPackages.keys[dropdown.value]
+  console.log(user.relocation_answer)
 }
 
 // Operating System Selection
@@ -217,6 +262,8 @@ for (value in operationSystems.all)
 function displayOperationSystemsFrom(dropdown)
 {
   $("#operating_system_selection_output").text(operationSystems.keys[dropdown.value]);
+  user.operating_system_answer = operationSystems.keys[dropdown.value]
+  console.log(user.operating_system_answer)
 }
 
 // Machine Type Selection
@@ -234,6 +281,8 @@ for (value in machineType.all)
 function displayMachineTypeFrom(dropdown)
 {
   $("#machine_type_selection_output").text(machineType.keys[dropdown.value]);
+  user.machine_type_answer = machineType.keys[dropdown.value]
+  console.log(user.machine_type_answer)
 }
 
 // Monitors Selection
@@ -251,6 +300,8 @@ for (value in monitors.all)
 function displayMonitorsFrom(dropdown)
 {
   $("#monitor_selection_output").text(monitors.keys[dropdown.value]);
+  user.monitor_answer = monitors.keys[dropdown.value]
+  console.log(user.monitor_answer)
 }
 
 // Level Selection
@@ -268,6 +319,8 @@ for (value in level.all)
 function displayLevelFrom(dropdown)
 {
   $("#level_selection_output").text(level.keys[dropdown.value]);
+  user.level_answer = level.keys[dropdown.value]
+  console.log(user.level_answer)
 }
 
 // Training Type Selection
@@ -285,6 +338,8 @@ for (value in trainingType.all)
 function displayTrainingTypeFrom(dropdown)
 {
   $("#training_type_selection_output").text(trainingType.keys[dropdown.value]);
+  user.training_type_answer = trainingType.keys[dropdown.value]
+  console.log(user.training_type_answer)
 }
 
 // Paid Time Off Selection
@@ -302,7 +357,96 @@ for (value in paidTimeOff.all)
 function displayPaidTimeOffFrom(dropdown)
 {
   $("#pto_selection_output").text(paidTimeOff.keys[dropdown.value]);
+  user.pto_answer = paidTimeOff.keys[dropdown.value]
+  console.log(user.pto_answer)
 }
+
+// Compare the user inputs with job requirements 
+var numberOfMatches = 0;
+
+function answerAll() {
+  if (user.employment_answer == "") {
+    alert("Please select an employment answer")
+    return
+  } 
+  if (user.experience_answer == "") {
+    alert("Please select experience")
+    return
+  }
+  if (user.companysize_answer == "") {
+    alert("Please select company size")
+    return
+  }
+  if (user.versioncontrol_answer == "") {
+    alert("Please select version control")
+    return
+  } 
+  if (user.issuetracker_answer == "") {
+    alert("Please select issue tracker")
+    return
+  } 
+  if (user.buildserver_answer == "") {
+    alert("Please select build server")
+    return
+  } 
+  if (user.codeanalysis_answer == "") {
+    alert("Please select code analysis")
+    return
+  } 
+  if (user.knowledgerepo_answer == "") {
+    alert("Please select knowledge repository")
+    return
+  } 
+  if (user.traveloptions_answer == "") {
+    alert("Please select travel option")
+    return
+  } 
+  if (user.schedule_answer == "") {
+    alert("Please select schedule")
+    return
+  } 
+  if (user.remotework_answer == "") {
+    alert("Please select an remote work preference")
+    return
+  } 
+  if (user.relocation_answer == "") {
+    alert("Please select relocation preference")
+    return
+  }
+  if (user.operating_system_answer == "") {
+    alert("Please select operating system")
+    return
+  }  
+  if (user.machine_type_answer == "") {
+    alert("Please select machine type")
+    return
+  } 
+  if (user.monitor_answer == "") {
+    alert("Please select monitor preference")
+    return
+  } 
+  if (user.level_answer == "") {
+    alert("Please select level")
+    return
+  } 
+  if (user.training_type_answer == "") {
+    alert("Please select training type")
+    return
+  } 
+  if (user.pto_answer == "") {
+    alert("Please select paid time off preference ")
+    return
+  }
+  console.log("function is working")
+}
+
+// function compareUserToJob() {
+//   if user.employment_answer = "Permanent" {
+//     numberOfMatches + 1
+//     console.log(numberOfMatches)
+//   }
+// }
+
 
 // Original Gridium code
 var job = {
@@ -399,3 +543,19 @@ function PTO() { return enumerate("Accrued", "Unlimited") }
 
 // https://github.com/RougeWare/Micro-JS-Enum/tree/master/lib
 function enumerate() { v=arguments;s={all:[],keys:v};for(i=v.length;i--;)s[v[i]]=s.all[i]=v[i];return s };
+
+
+
+// var user = {
+//     employment_answer:
+//     experience_answer:
+//     companysize_answer:
+//     versioncontrol_answer:
+//     issuetracker_answer:
+//     buildserver_answer:
+//     codeanalysis_answer:
+//     knowledgerepo_answer:
+//     traveloptions_answer:
+//     schedule_answer:
+//     remotework_answer:
+// }
